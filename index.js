8b2497c4d0bc2d9c95444a2e051fd17468d04972
andrew <andrew_hoffman262@outlook.com>
2023-09-30 05:36:03 -0600

    define functions to pass all npm tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee, [key]: value };
+    return newEmployee;
+}
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeClone = { ...employee };
+  
+    delete employeeClone[key];
+  
+    return employeeClone
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
